08675848557e828ad50ad100a7a2e898246ab058
maozhongyu <mao.zhongyu@qianka.com>
2021-03-31 23:59:17 +0800

    自定义手机验证器

--- user_web/initalize/validate.go
+++ user_web/initalize/validate.go
@@ -3,9 +3,6 @@ package initalize
 import (
 	"bff/user_web/global"
 	"fmt"
-	"reflect"
-	"strings"
-
 	"github.com/gin-gonic/gin/binding"
 	en2 "github.com/go-playground/locales/en"
 	zh2 "github.com/go-playground/locales/zh"
@@ -13,12 +10,16 @@ import (
 	"github.com/go-playground/validator/v10"
 	en_translations "github.com/go-playground/validator/v10/translations/en"
 	zh_translations "github.com/go-playground/validator/v10/translations/zh"
+	"reflect"
+	"strings"
 )
 
 var uni *ut.UniversalTranslator
 
 func InitTrans(locale string) (err error) {
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+
+		//采用tag  json的标签名
 		v.RegisterTagNameFunc(func(fld reflect.StructField) string {
 			name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
 			if name == "-" {
--- user_web/main.go
+++ user_web/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"bff/user_web/global"
 	"fmt"
-
+	"github.com/gin-gonic/gin/binding"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
 	"go.uber.org/zap"
 
 	"bff/user_web/initalize"
+	myvalidator "bff/user_web/validator"
 )
 
 func main() {
@@ -18,6 +21,19 @@ func main() {
 	//初始化验证翻译
 	if err := initalize.InitTrans("zh"); err != nil {
 		panic(err.Error())
+	}
+
+	//注册自定义验证器
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		_ = v.RegisterValidation("mobile", myvalidator.ValidateMobile)
+		//翻译问题
+		// https://github.com/go-playground/validator/blob/v9/_examples/translations/main.go#L105
+		_ = v.RegisterTranslation("mobile", global.Trans, func(ut ut.Translator) error {
+			return ut.Add("mobile", "{0} 非法的手机号码!", true) // see universal-translator for details
+		}, func(ut ut.Translator, fe validator.FieldError) string {
+			t, _ := ut.T("mobile", fe.Field())
+			return t
+		})
 	}
 	//初始化路由
 	Router := initalize.InitRouters()
