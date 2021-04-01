e0d245f29dffe8e6ee8983bae88e0a7fec66d33d
maozhongyu <mao.zhongyu@qianka.com>
2021-04-01 21:51:08 +0800

    health check 服务

--- user_web/initalize/router.go
+++ user_web/initalize/router.go
@@ -4,10 +4,17 @@ import (
 	"bff/user_web/middlewares"
 	router2 "bff/user_web/router"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func InitRouters() *gin.Engine {
 	router := gin.Default()
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    200,
+			"success": true,
+		})
+	})
 	router.Use(middlewares.Cors())
 	ApiGroup := router.Group("/v1")
 	router2.InitUserRouter(ApiGroup)
