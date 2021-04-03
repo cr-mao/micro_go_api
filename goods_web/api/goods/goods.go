a5c5e3a80f4793d6ee5396e5c15a19593bd4ca14
maozhongyu <mao.zhongyu@qianka.com>
2021-04-03 14:52:17 +0800

    goods api

--- goods_web/api/goods/goods.go
+++ goods_web/api/goods/goods.go
@@ -1,7 +1,34 @@
 package goods
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"net/http"
+
+	"bff/goods_web/api"
+	"bff/goods_web/global"
+	"bff/goods_web/proto"
+)
 
 func List(c *gin.Context) {
+	request := &proto.GoodsFilterRequest{}
+	request.TopCategroy = 1
+	isHot := c.DefaultQuery("ih", "0")
+	if isHot == "1" {
+		request.IsHot = true
+	}
+
+	rsp, err := global.GoodsSrvClient.GoodsList(context.Background(), request)
+	if err != nil {
+		zap.S().Errorw("查询商品失败")
+		api.HandleGrpcErrorToHttp(err, c)
+		return
+	}
+	result := map[string]interface{}{
+		"total": rsp.Total,
+		"goods": rsp.Goods,
+	}
+	c.JSON(http.StatusOK, result)
 
 }
